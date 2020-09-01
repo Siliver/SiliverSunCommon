49812a5932e8b17430fa2b3220a6000e3d94ba6f
Siliver <36077605+Siliver@users.noreply.github.com>
2020-09-01 23:47:17 +0800

    添加URI，TCP 工具类

---
+++ SiliverSun/TcpTool/TcpHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiliverSun.TcpTool
+{
+    public class TcpHelper
+    {
+        private const int ReadBufferSize = 1024;
+
+        public static async Task<string> RequestHtmlAsync(string hostname) {
+            try
+            {
+                using var client = new TcpClient();
+                await client.ConnectAsync(hostname, 20);
+                NetworkStream stream = client.GetStream();
+
+                string header = $"GET/HTTP/1.1\r\nHost:{hostname}:80\r\nConnection:close\r\n\r\n";
+                byte[] buffer = Encoding.UTF8.GetBytes(header);
+                await stream.WriteAsync(buffer, 0, buffer.Length);
+                await stream.FlushAsync();
+                var ms = new MemoryStream();
+                buffer = new byte[ReadBufferSize];
+                int read = 0;
+                do
+                {
+                    read = await stream.ReadAsync(buffer, 0, ReadBufferSize);
+                    ms.Write(buffer, 0, read);
+                    Array.Clear(buffer, 0, buffer.Length);
+                } while (read > 0);
+                ms.Seek(0, SeekOrigin.Begin);
+                var reader = new StreamReader(ms);
+                return reader.ReadToEnd();
+            }
+            catch (Exception ex) { 
+            
+            }
+        }
+    }
+}
