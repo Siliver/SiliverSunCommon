b4dcbc17e18d770aaf1c151a8e9e142af4ca6fe6
银鹏 孙 <765577573@qq.com>
2020-09-07 16:36:21 +0800

    1

--- SiliverSun/MongoDBHelper/MongoDBSingleClient.cs
+++ SiliverSun/MongoDBHelper/MongoDBSingleClient.cs
@@ -1,7 +1,4 @@
 ﻿using MongoDB.Driver;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace SiliverSun.MongoDBHelper
 {
@@ -11,6 +8,10 @@ namespace SiliverSun.MongoDBHelper
 
         public MongoClient mongoclient{ get => _mongoclient ?? new MongoClient(); }
 
+        /// <summary>
+        /// 单地址链接
+        /// </summary>
+        /// <param name="connecturl"></param>
         public MongoDBSingleClient(string connecturl) {
             if (connecturl != null)
             {
