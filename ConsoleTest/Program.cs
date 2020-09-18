c2c0a79aa1674e99102d37f143f066429718a905
银鹏 孙 <765577573@qq.com>
2020-09-18 13:56:55 +0800

    1.添加

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dd = DateTime.ParseExact("202104280800", "yyyyMMddHHmm", null).ToString("yyyy-MM-dd HH:mm:ss");
+            Console.WriteLine(dd);
+        }
+    }
+}
