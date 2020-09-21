71cd4ed505b1d79f3651e27be396c572d180fa98
银鹏 孙 <765577573@qq.com>
2020-09-21 15:25:26 +0800

    1

--- SiliverSun/RedisTool/SiliverRadisHelper.cs
+++ SiliverSun/RedisTool/SiliverRadisHelper.cs
@@ -1,4 +1,5 @@
 ﻿using StackExchange.Redis;
+using System;
 
 namespace SiliverSun.RedisTool
 {
@@ -7,6 +8,8 @@ namespace SiliverSun.RedisTool
     /// </summary>
     public class SiliverRadisHelper
     {
+        #region 私有的Radis的配置
+
         private string _redisname;
 
         private string _redisurl;
@@ -17,16 +20,157 @@ namespace SiliverSun.RedisTool
 
         private ConnectionMultiplexer _redis;
 
-        public SiliverRadisHelper(string url, string port, string name, string password){
-            _redisurl = url;
-            _redisport = port;
-            _redisname = name;
-            _redispassword = password;
+        #endregion
 
+        /// <summary>
+        /// Redis帮助类
+        /// </summary>
+        /// <param name="url">redis地址</param>
+        /// <param name="port">resdis端口</param>
+        /// <param name="name">redis名称</param>
+        /// <param name="password">redis密码</param>
+        public SiliverRadisHelper(string url, string port, string name, string password){
+
+            if (_redisurl == url || _redisport == port || _redisname == name || _redispassword == password)
+            {
+                if (_redis == null)
+                {
+                    _redis = ConnectionMultiplexer.Connect($"{_redisurl}:{_redisport},password={_redispassword},DefaultDatabase={_redisname}");
+                }
+            }
+            else {
+                _redisurl = url;
+                _redisport = port;
+                _redisname = name;
+                _redispassword = password;
+
+                if (_redis != null)
+                {
+                    _redis.Dispose();
+                }
+                _redis = ConnectionMultiplexer.Connect($"{_redisurl}:{_redisport},password={_redispassword},DefaultDatabase={_redisname}");
+            }
+        }
+
+        /// <summary>
+        /// 进行Hash置的添加
+        /// </summary>
+        /// <param name="objectname"></param>
+        /// <param name="keystring"></param>
+        /// <param name="valuestring"></param>
+        /// <returns></returns>
+        public bool SetHashData(string objectname, string keystring, string valuestring, int dbindex=0)
+        {
+
+            if (_redis == null) {
+                return false;
+            }
+            var database = _redis.GetDatabase(dbindex);
+            if (database == null)
+            {
+                return false;
+            }
+
+            //进行Hash数据的添加
+            var flag = database.HashSetAsync(objectname, keystring, valuestring, When.NotExists).Result;
+
+            return flag;
+
+        }
+
+        /// <summary>
+        /// 根据表名和主键进行字段值的获取
+        /// </summary>
+        /// <param name="objectname">表名</param>
+        /// <param name="keystring">主键名称</param>
+        /// <param name="dbindex">要查询的缓存索引</param>
+        /// <param name="expiry">缓存的过期时间</param>
+        /// <returns></returns>
+        public string GetHashData(string objectname, string keystring, int dbindex = 0, DateTime? expiry = null)
+        {
             if (_redis == null)
             {
-                _redis= ConnectionMultiplexer.Connect($"{_redisurl}:{_redisport}");
+                return string.Empty;
             }
+            var database = _redis.GetDatabase(dbindex);
+            if (database == null)
+            {
+                return string.Empty;
+            }
+
+            if(string.IsNullOrWhiteSpace(objectname) || string.IsNullOrWhiteSpace(keystring))
+            {
+                return string.Empty;
+            }
+
+            //进行Hash数据的获取
+            var hashdata = database.HashGet(objectname, keystring);
+
+            if(expiry!=null)
+            {
+                database.KeyExpireAsync(objectname, expiry);
+            }
+
+            return hashdata;
+        }
+
+        /// <summary>
+        /// 根据表名和主键删除对应的键
+        /// </summary>
+        /// <param name="objectname">表明</param>
+        /// <param name="keystring">键名</param>
+        /// <param name="dbindex">索引库</param>
+        /// <returns></returns>
+        public bool DeleteHashData(string objectname, string keystring, int dbindex = 0) {
+            if (_redis == null)
+            {
+                return false;
+            }
+            var database = _redis.GetDatabase(dbindex);
+            if (database == null)
+            {
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(objectname) || string.IsNullOrWhiteSpace(keystring))
+            {
+                return false;
+            }
+
+            //进行Hash数据的获取
+            var hashdata = database.HashDeleteAsync(objectname, keystring).Result;
+
+            return hashdata;
+        }
+
+        /// <summary>
+        /// 根据表名和主键删除对应的键
+        /// </summary>
+        /// <param name="objectname">表明</param>
+        /// <param name="keystring">键名</param>
+        /// <param name="dbindex">索引库</param>
+        /// <returns></returns>
+        public bool DeleteHash(string objectname, int dbindex = 0)
+        {
+            if (_redis == null)
+            {
+                return false;
+            }
+            var database = _redis.GetDatabase(dbindex);
+            if (database == null)
+            {
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(objectname))
+            {
+                return false;
+            }
+
+            //进行Hash数据的获取
+            var hashdata = database.KeyDeleteAsync(objectname).Result;
+
+            return hashdata;
         }
     }
 }
--- SingleHttpServer/Program.cs
+++ SingleHttpServer/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using SiliverSun.RedisTool;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SingleHttpServer
@@ -7,7 +10,30 @@ namespace SingleHttpServer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SiliverRadisHelper srh = new SiliverRadisHelper("117.78.43.23", "6000", "8", "88691111");
+            int i = 1;
+
+            var data = new {
+                insurance_order_code= "BX25200918000076",
+                order_product_ids=new List<string> {
+                    "BX2520091800007600"
+                }
+            };
+
+            var setresult = srh.SetHashData("BX25200918000076", i.ToString(), JsonConvert.SerializeObject(data), 8);
+
+            var readresult = srh.GetHashData("BX25200918000076", i.ToString(), 8);
+
+            Console.WriteLine(readresult);
+
+
+            var deleteresult = srh.DeleteHash("BX25200918000076", 8);
+
+            Console.WriteLine(deleteresult);
+
+            readresult = srh.GetHashData("BX25200918000076", i.ToString(), 8);
+
+            Console.WriteLine(readresult);
         }
 
         public static async Task StartServerAsync(params string[] prefixes) {
