dd6147cf5aae06b74b338425e6aeebf1e4556645
银鹏 孙 <765577573@qq.com>
2020-09-21 11:06:03 +0800

    1

---
+++ SiliverSun/RedisTool/SiliverRadisHelper.cs
@@ -0,0 +1,32 @@
+﻿using StackExchange.Redis;
+
+namespace SiliverSun.RedisTool
+{
+    /// <summary>
+    /// Redis帮助类
+    /// </summary>
+    public class SiliverRadisHelper
+    {
+        private string _redisname;
+
+        private string _redisurl;
+
+        private string _redispassword;
+
+        private string _redisport;
+
+        private ConnectionMultiplexer _redis;
+
+        public SiliverRadisHelper(string url, string port, string name, string password){
+            _redisurl = url;
+            _redisport = port;
+            _redisname = name;
+            _redispassword = password;
+
+            if (_redis == null)
+            {
+                _redis= ConnectionMultiplexer.Connect($"{_redisurl}:{_redisport}");
+            }
+        }
+    }
+}
